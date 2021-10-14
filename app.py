48b550652c514e01171472317cf92392eabe7675
Sam Burkhard <samryanburk@icloud.com>
2021-10-14 02:06:20 -0400

    experimented with audio-analysis api and new model types

--- app.py
+++ app.py
@@ -35,57 +35,98 @@ headers = {
 # Variables
 
 song_list = []
+song_id_list = []
 
-song_count = 0
+total_song_count = 0
+playlist_count = 0
 
 isState = False
 
 # Check url
 
-playlist_path = urlparse(str(playlist_url)).path
-playlist_id = playlist_path.split('/')[-1]
+playlists = [
+    # 70s Music
+    "https://open.spotify.com/playlist/37i9dQZF1DWTJ7xPn4vNaz?si=c6ff259b874944ec",
+    "https://open.spotify.com/playlist/37i9dQZF1DWYOqWoDP009i?si=41571d2ccefc4d74",
+    "https://open.spotify.com/playlist/37i9dQZF1DWWwzidNQX6jx?si=65f4e1ceccfb4db5",
+    "https://open.spotify.com/playlist/37i9dQZF1DWTTn6daQVbOa?si=def43b9ad24c495b",
+    "https://open.spotify.com/playlist/37i9dQZF1DWWg3YRMu4AEF?si=a416c4d987234ab3",
+    "https://open.spotify.com/playlist/37i9dQZF1DWSWNiyXQAvbl?si=9724070e46a9448b",
 
-spotify_playlist = requests.get(api_url + 'playlists/' + playlist_id + '/tracks', headers=headers).json()
+    # 10s Music
+    "https://open.spotify.com/playlist/37i9dQZF1DX5Ejj0EkURtP?si=fe86b552d4d54926",
+    "https://open.spotify.com/playlist/37i9dQZF1DX99DRG9N39X3?si=3372db4e85b84963",
+    "https://open.spotify.com/playlist/37i9dQZF1DWXbttAJcbphz?si=26eb9dd87a20492e",
+    "https://open.spotify.com/playlist/37i9dQZF1DX1uHCeFHcn8X?si=fa21557de9cd4845",
+    "https://open.spotify.com/playlist/37i9dQZF1DWSMyFeHM3son?si=c168e276b3194b90",
+    "https://open.spotify.com/playlist/37i9dQZF1DWVTfbQdQ8l7H?si=2917239b9586421a"
+]
 
-if spotify_playlist:
-    if 'items' in spotify_playlist:
-        for item in spotify_playlist['items']:
-            if 'track' in item:
-                if 'id' in item['track']:
-                    try:
-                        spotify_song = requests.get(api_url + 'audio-features/' + item['track']['id'], headers=headers).json()
+for playlist in playlists:
 
-                        if state == 1:
-                           isState = True
+    playlist_path = urlparse(str(playlist)).path
+    playlist_id = playlist_path.split('/')[-1]
 
-                        new_song = [
-                            spotify_song['danceability'],
-                            spotify_song["energy"],
-                            spotify_song["key"],
-                            spotify_song["loudness"],
-                            spotify_song["speechiness"],
-                            spotify_song["acousticness"],
-                            spotify_song["instrumentalness"],
-                            spotify_song["liveness"],
-                            spotify_song["valence"],
-                            spotify_song["tempo"],                       
-                            spotify_song["time_signature"],
-                            isState
-                        ]
-                        song_list.append(new_song)
-                        print('Scraped Song ' + str(song_count+1) + ' | ' + str(song_list[song_count]))
-                        song_count += 1
-                    except TypeError:
-                        print('⚠️  Error Fetching Song ' + str(song_count+1))
+    spotify_playlist = requests.get(api_url + 'playlists/' + playlist_id + '/tracks', headers=headers).json()
+
+    if spotify_playlist:
+
+        song_count = 0
+
+        if 'items' in spotify_playlist:
+            for item in spotify_playlist['items']:
+                if 'track' in item:
+                    if 'id' in item['track']:
+                        if not item['track']['id'] in song_id_list:
+                            song_id_list.append(item['track']['id'])
+                            try:
+                                spotify_song = requests.get(api_url + 'audio-analysis/' + item['track']['id'], headers=headers).json()
+
+                                # if state == 1:
+                                #     isState = True
+                                if playlist_count <= 5:
+                                    isState = True
+
+                                new_song = [
+                                    spotify_song['track']['end_of_fade_in'],
+                                    spotify_song['track']['start_of_fade_out'],
+                                    spotify_song["track"]['loudness'],
+                                    spotify_song["track"]['tempo'],
+                                    spotify_song["track"]['tempo_confidence'],
+                                    spotify_song["track"]['time_signature'],
+                                    spotify_song["track"]['time_signature_confidence'],
+                                    spotify_song["track"]['key'],
+                                    spotify_song["track"]['key_confidence'],
+                                    spotify_song["track"]['mode'],                       
+                                    spotify_song["track"]['mode_confidence'],
+                                    spotify_song['bars'],
+                                    spotify_song['beats'],
+                                    spotify_song['sections'],
+                                    spotify_song['segments'],
+                                    spotify_song['tatums'],
+                                    isState
+                                ]
+                                song_list.append(new_song)
+                                print('Scraped Song ' + str(song_count+1) + "/" + str(len(spotify_playlist['items'])) + " | " + str(total_song_count) + " Cumulative")
+                                song_count += 1
+                            except TypeError:
+                                print('⚠️  Error Fetching Song ' + str(song_count+1))
+                        else:
+                            print('Already Fetched Song')
+    
+    playlist_count += 1
+    total_song_count += song_count
+    print("Finished Playlist " + str(playlist_count) + " ==========================================")
+    print("Completed " + str(total_song_count) + " songs so far")
 
 
-print('📦 Creating CSV for ' + str(song_count) +' songs...')
+print('📦 Creating CSV for ' + str(total_song_count) +' songs...')
 if file_output_name == 0:
     file_output_name = 'songs'
 
 with open('./' + str(file_output_name) + '.csv', 'w') as f:
     writer = csv.writer(f, delimiter=',')
-    writer.writerow(['danceability', 'energy', 'key', 'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'isRetro'])
+    writer.writerow(['end_of_fade_out', 'start_of_fade_out', 'loudness', 'tempo', 'tempo_confidence', 'time_signature', 'time_signature_confidence', 'key', 'key_confidence', 'mode', 'mode_confidence', 'bars', 'beats', 'sections', 'segments', 'tatums', 'isState'])
     writer.writerows(song_list)
 f.close()
 
